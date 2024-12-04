82371f78b0ce9c311c2ecb688b8df9d9c5aaedee
Raghav Mittal <mittalraghav45@gmail.com>
2024-12-04 19:39:13 +0000

    adding reducer

--- redux/index.js
+++ redux/index.js
@@ -7,7 +7,28 @@ function buyCake(){
     }
 }
 
+const initialState=
+{
+    numberOfCakes:10
+}
+
+const reducer(state=initialState,action)=>
+{
+    switch(action.type)
+    {
+        case BUY_CAKE:
+        return{
+            ...state,
+            numberOfCakes:state.numberOfCakes-1
+        }
+        default:
+            return state;
+    }
+}
+
 
 
 
 //An action is an object with a type property
+
+//reducers specify how the app's sytate changes in resoonse to actions sent to the store, its like (prevState,action)=>newState  , we get a neww state returned
