af3f14c76298b5fca2a1220f76b84cdd08f06fe5
Raghav Mittal <mittalraghav45@gmail.com>
2024-12-06 00:31:29 +0000

    adding reducer for icecream

--- redux/index.js
+++ redux/index.js
@@ -1,4 +1,8 @@
+const redux=require('redux');
+const createStore= redux.createStore
+
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake(){
     return{
@@ -6,13 +10,19 @@ function buyCake(){
         info:'First redux action'
     }
 }
-
-const initialState=
-{
-    numberOfCakes:10
+function buyIceCream(){
+    return{
+        type:BUY_ICECREAM,
+        info:'First ICREAM action'
+    }
 }
 
-const reducer(state=initialState,action)=>
+const initialState={
+    numberOfCakes:10,
+    numberOfIceCream:10
+}
+
+const reducer=(state=initialState,action)=>
 {
     switch(action.type)
     {
@@ -21,14 +31,29 @@ const reducer(state=initialState,action)=>
             ...state,
             numberOfCakes:state.numberOfCakes-1
         }
+        case BUY_ICECREAM:
+            return{
+                ...state,
+                numberOfIceCream:state.numberOfIceCream-1
+            }
         default:
             return state;
     }
 }
 
+const store=createStore(reducer);
 
+console.log('initial state',store.getState());
 
+const unsubscribe=store.subscribe(()=>console.log('updated state ',store.getState())) 
+
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyCake())
+unsubscribe();
 
 //An action is an object with a type property
 
 //reducers specify how the app's sytate changes in resoonse to actions sent to the store, its like (prevState,action)=>newState  , we get a neww state returned
+
+// refer this img for steps: // redux-store-steps
